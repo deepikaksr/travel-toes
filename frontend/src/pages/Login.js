26f21656363827193bce06ce5db7bda6cb5ad6da
deepikaksr <115573746+deepikaksr@users.noreply.github.com>
2024-10-05 13:48:18 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,3 @@
-// Login.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -27,18 +26,27 @@ const Login = () => {
       }
     } catch (error) {
       console.error('Error during login:', error);
-      alert('An error occurred while logging in. Please try again.'); // General error message
+      alert('An error occurred while logging in. Please try again.');
     }
   };
 
   return (
-    <div>
-      <h2 className="text-center">Login</h2>
-      <form className="mb-3" onSubmit={handleLogin}>
-        <input className="form-control" type="email" placeholder="Email" required />
-        <input className="form-control mt-2" type="password" placeholder="Password" required />
-        <button className="btn btn-primary mt-3" type="submit">Login</button>
-      </form>
+    <div style={{ display: 'flex', justifyContent: 'center', height: '50vh' }}>
+      <div>
+        <br /><br />
+        <h2 className="text-center">Login</h2>
+        <form className="mb-3" onSubmit={handleLogin} style={{ display: 'flex', flexDirection: 'column', width: '400px' }}>
+          <input className="form-control" type="email" placeholder="Email" required style={{ marginBottom: '10px' }} />
+          <input className="form-control" type="password" placeholder="Password" required style={{ marginBottom: '30px' }} />
+          <button className="btn btn-primary" type="submit" style={{ width: '100px', margin: '0 auto' }}>Login</button>
+        </form>
+        <p className="text-center mt-3">
+          New user?{' '}
+          <span onClick={() => navigate('/signup')} style={{ color: 'blue', cursor: 'pointer' }}>
+            Sign up here
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
